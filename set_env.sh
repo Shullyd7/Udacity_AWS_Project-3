357443a63fee601796c36163378afc32b2c2f1e6
Shulammite Odde <shullyd7@gmail.com>
2022-07-09 01:47:18 +0100

    Update

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=shully
+export POSTGRES_PASSWORD=shullyrachy
+export POSTGRES_HOST=udagram-database.cvusprowxhek.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-shully
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
